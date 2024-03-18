ce4fd2730c4f53b7ff71690044cd1f655bf53f82
sairaj04 <sairaj04480@gmail.com>
2024-03-19 00:04:33 +0530

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -17,23 +17,10 @@ export default function About() {
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
-        After graduating with a degree in with a Bachelor's degree in{" "}
-        <span className="font-semibold">Computer Science. </span>
-        My journey in technology has been fueled by a relentless curiosity and a
-        desire to create <span className="font-semibold">innovative</span>{" "}
-        solutions. With over <span className="font-semibold">three years</span>{" "}
-        of practical experience, I've cultivated expertise in front-end and
-        back-end development, DevOps tools, cloud technologies, and Linux
-        systems.
-      </p>
-
-      <p>
-        Beyond coding, I'm an avid{" "}
-        <span className="font-semibold">tech enthusiast</span> who loves staying
-        updated with the latest industry trends. I thrive on new challenges and
-        believe in pushing the boundaries of what technology can achieve.
-      </p>
+      <p className="mb-3">After getting a <span className="font-semibold">Bachelor's degree in Computer Science</span>, I started my journey in technology. I am very curious and want to create <span className="font-semibold">new and innovative solutions</span>. I have <span className="font-semibold">one year</span> of practical experience working as a frontend developer.</p>
+      <p className="mb-3">In this time, I have learned <span className="font-semibold">React.js</span> for building user interfaces. I am also skilled in using <span className="font-semibold">Tailwind CSS</span> for styling websites. I have a good understanding of <span className="font-semibold">data structures</span>, which helps me write better code. I can <span className="font-semibold">deploy websites</span> using various tools.</p>
+      <p className="mb-3">Currently, I am pursuing a <span className="font-semibold">Master's degree in Computer Science</span> at Fullerton, CA. This is helping me gain deeper knowledge, especially in frontend development areas.</p>
+      <p className="mb-3">Apart from coding, I like technology and keeping up with the <span className="font-semibold">latest frontend trends</span> in the industry. I enjoy taking on new challenges and seeing how far I can push frontend technologies to do new things.</p>
     </motion.section>
   );
 }
